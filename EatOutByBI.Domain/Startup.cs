68da84d80c05cc40900a12f798c7770eadb7ddd3
Timbm3 <timbrinkmadsen@live.se>
2016-10-24 11:55:36 +0200

    Nya MVC3

---
+++ EatOutByBI.Domain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EatOutByBI.Domain.Startup))]
+namespace EatOutByBI.Domain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- EatOutByBI1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EatOutByBI1.MvcApplication" Language="C#" %>
